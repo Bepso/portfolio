9f1517c18730f9fa07de5b76c871324646c2d345
Sebastian Flinck Lindström <sefl@netlight.com>
2023-02-21 14:24:56 +0100

    Init commit

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -8,6 +8,7 @@ function Header() {
       </div>
       <div className="App-header-middle">
         <h1>I am Sebastian</h1>
+        <p>I am a passionate Software Engineer with a keen interest in building robust and scalable applications. Take a look at my portfolio below to see some of my recent work.</p>
       </div>
       <div className="App-header-right">
         <a href="#about">About</a>
