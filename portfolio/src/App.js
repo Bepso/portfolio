12e26dcf9ba341007db98ed9f43df38b3dc1ccfe
Sebastian Flinck Lindström <sefl@netlight.com>
2023-02-22 13:51:57 +0100

    Init projects section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import About from './components/About/About';
-import Projects from './components/Projects';
+import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 
 function App() {
---
+++ portfolio/src/components/Projects/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Projects.css'
+
+function Projects() {
+  return (
+    <div className='App-projects-parent'>
+      <section id="projects"className="App-projects">
+        <h2>My Projects</h2>
+        <p>Insert your text here...</p>
+      </section>
+    </div>
+    
+  );
+}
+
+export default Projects;
