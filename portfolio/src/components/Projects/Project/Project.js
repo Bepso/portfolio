606c06f46896ca664d56fe2d5a0132599146c786
Sebastian Flinck Lindström <sefl@netlight.com>
2023-03-02 13:59:58 +0100

    Add link to projects

--- portfolio/src/components/Projects/Project/Project.js
+++ portfolio/src/components/Projects/Project/Project.js
@@ -5,16 +5,15 @@ import githubLogo from '../../../assets/project-images/GitHub.png'
 function Project(props) {
   const { title, description, githubLink, imageSrc } = props;
   return (
-    <div className="project-container">
+    <a href={githubLink} target="_blank" rel="noopener noreferrer" className="project-link">
+      <div className="project-container">
         <div className='project-description'>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <a href={githubLink} target="_blank" rel="noopener noreferrer">
-            <img src={githubLogo} alt="GitHub" className="github-logo"/>
-            </a>
+          <h3>{title}</h3>
+          <p>{description}</p>
+          <img src={githubLogo} alt="GitHub" className="github-logo"/>
         </div>
-      <img src={imageSrc} alt={description} className="project-image"/>
-    </div>
+      </div>
+    </a>
   );
 }
 
